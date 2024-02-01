d84f1769e808573b5b4437e49adc5b4c8ba132b5
Priyanshu-Batham <priyanshubatham24733@gmail.com>
2024-02-01 18:12:51 +0530

    matrixForward

--- bca/class&Obj/matrixTranspose.java
+++ bca/class&Obj/matrixTranspose.java
@@ -2,12 +2,14 @@ import java.util.*;
 
 class matrixTranspose{
     public static void main(String args[]){
-        Transpose mat1 = new Transpose(3, 3);
-        Transpose mat2 = new Transpose(3, 3);
+        Transpose mat1 = new Transpose(4, 4);
 
         mat1.input();
-        mat2.rotate(mat1);
-        mat2.display();
+        mat1.display();
+        // mat1.forRow();
+        // mat1.display();
+        mat1.forCol();
+        mat1.display();
     }
 }
 
@@ -39,10 +41,12 @@ class Transpose{
         }
     }
 
-    void rotate(Transpose mat){
-        for(int i = 0; i<mat.m; i++){
-            for(int j = 0; j<mat.n; j++){
-                a[i][j] = mat.a[j][i];
+    void rotate(){
+        for(int i = 0; i<m; i++){
+            for(int j = i; j<n; j++){
+                int temp = a[i][j];
+                a[i][j] = a[j][i];
+                a[j][i] = temp;
             }
         }
     }
@@ -50,10 +54,30 @@ class Transpose{
     void display(){
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
-                System.out.print(a[i][j]);
+                System.out.print(a[i][j] + " ");
             }
             System.out.println("");
         }
-        System.out.print("Donee");
+        System.out.println("-----------------");
+    }
+
+    void forRow(){
+        for(int i = 0; i<m ; i++){
+            int temp = a[i][n-1];
+            for(int j = n-2; j>=0; j--){
+                a[i][j+1] = a[i][j];
+            }
+            a[i][0] = temp;
+        }
+    }
+
+    void forCol(){
+        for(int i = 0; i<n; i++){
+            int temp = a[0][i];
+            for(int j = 1; j<m; j++){
+                a[j-1][i] = a[j][i];
+            }
+            a[m-1][i] = temp;
+        }
     }
 }
